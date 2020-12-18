54eddc30ec6016c6f71e92433e1f391a1ed01d52
silverest <silverest12@gmail.com>
2020-12-18 12:44:13 +0100

    added part 2 day 11

--- day11.scala
+++ day11.scala
@@ -1,34 +1,57 @@
 object Day11 {
 
-  type Seats = Array[Array[Char]]
+  case class Point (x: Int, y: Int) {
+    def +(that: Point): Point = Point (x + that.x, y + that.y)
+  }
 
-  def checkAdjSeat (xs: Seats): Int =
-    xs.map(_.count(_ == '#')).sum
+  val directions = for (y <- -1 to 1; x <- -1 to 1; if x != 0 || y != 0) yield Point(x, y)
 
-  def checkAlignedSeats (xss: Seats): Int = ???
+  type Seats = Vector[Vector[Option[Boolean]]]
 
-  def nextSeats (xss: Seats): Seats =
-    xss.zipWithIndex.map {xs =>
-      xs._1.zipWithIndex.map {x =>
-        lazy val occAdjSeats = checkAdjSeat (xss.slice(xs._2-1, xs._2+2).map(ys => ys.slice(x._2-1, x._2+2)))
-        (x._1, occAdjSeats) match {
-          case ('L', 0) => '#'
-          case ('#', adj) if adj > 4 => 'L'
-          case _ => x._1
+  /*val map: Seats = 
+    scala.io.Source.fromFile("input/day11.txt").getLines()
+    .map{x => 
+      x.toVector.map{y => 
+        y match{
+          case '#' => Some(true)
+          case 'L' => Some(false)
+          case _ => None
         }
       }
+    }.toVector*/
+
+  def inBounds(map: Seats, p: Point): Boolean = p.x >= 0 && p.x < map.head.size && p.y >= 0 && p.y < map.size
+
+  def fixedPoint(map: Seats)(adjacency: (Point => Option[Boolean]) => (Point, Point) => Boolean, turnover: Int): Int = {
+    def iterate(state: Seats): Int = {
+      val next = state.zipWithIndex.map(r => r._1.zipWithIndex.map { e =>
+        val p = Point(e._2, r._2)
+        val occupied = directions.count(d => adjacency(q => state(q.y)(q.x))(p + d, d))
+        e._1.map(s => if (s) occupied < turnover else occupied == 0)
+      })
+      if (next == state) state.flatten.count(_.exists(identity)) else iterate(next)
     }
+    iterate(map)
+  }
 
-  def iCouldJustUseWhile (xss: Seats, last: Seats) : Seats = 
-    if(last.map(_.mkString).sameElements(xss.map(_.mkString))) xss else iCouldJustUseWhile(nextSeats(xss), xss)
+  def scan(map: Seats)(f: Point => Option[Boolean])(p: Point, direction: Point): Boolean =
+    inBounds(map, p) && f(p).getOrElse(scan(map)(f)(p + direction, direction))
 
-  def part1 (inp: Seats) = 
-    checkAdjSeat(iCouldJustUseWhile(inp.map(_.map(x => if (x == 'L') '#' else x)), inp))
+  def part1 (inp: Seats) = fixedPoint(inp)(f => (p, _) => inBounds(inp, p) && f(p).exists(identity), 4)
 
-  def part2 (inp: Seats) = ???
+  def part2 (inp: Seats) = fixedPoint(inp)(scan(inp) _, 5)
 
   def main (args: Array[String]): Unit = {
-    val inp = scala.io.Source.fromFile("input/day11.txt").getLines().map(_.toCharArray()).toArray
+    val inp = scala.io.Source.fromFile("input/day11.txt").getLines()
+      .map{x =>
+        x.toVector.map{
+        case '#' => Some(true)
+        case 'L' => Some(false)
+        case _   => None
+        }
+      }.toVector
+
     println(part1(inp))
+    println(part2(inp))
   }
 }
---
+++ day16.scala
@@ -0,0 +1,5 @@
+object day16 {
+  def main (args: Array[String]): Unit = {
+    val inp = scala.io.Source("input/day16.txt").getLines()
+  }
+}
